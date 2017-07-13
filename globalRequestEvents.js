4cd435b1ed17ab34b12ca3236165a483235b96d7
Victor Herrera <hmvictor@users.noreply.github.com>
2017-07-12 22:34:02 -0500

    Update globalRequestEvents.js

--- globalRequestEvents.js
+++ globalRequestEvents.js
@@ -1,2 +1,21 @@
 (function(){
+  function createRequestEvent(eventName, request) {
+    var event=new Event("Event");
+    event.init(eventName, true, true);
+    return event;
+  }
+  function fireEvent(event) {
+    document.dispatchEvent(event);
+  }
+  var proxied=XMLHttpRequest.prototype.send;
+  XMLHttpRequest.prototype.send=function() {
+    var request=this;
+    function finishedEventHandler() {
+      fireEvent(createRequestEvent("requestFinished", request));
+    }
+    request.addEventListener("load", finishedEventHandler);
+    request.addEventListener("error", finishedEventHandler);
+    fireEvent(createRequestEvent(request, "requestStarted"));
+    return proxied.apply(request, arguments);
+  };
 })();
