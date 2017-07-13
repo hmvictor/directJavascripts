ffd6d42f975662127da5660ce82085a31cb43b99
Victor Herrera <hmvictor@users.noreply.github.com>
2017-07-12 21:36:11 -0500

    Update httpHiddenMethod.js
    First and quick implementation.

--- httpHiddenMethod.js
+++ httpHiddenMethod.js
@@ -1,2 +1,16 @@
 (function(){
+  var proxied=XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open=function(method, url, user, password) {
+    var m=method;
+    var u=url;
+    if(m === "DELETE" || m === "PUT") {
+        m="POST";
+        if(u.indexOf("?") === -1){
+           u+="?_method="+method;
+        }else{
+           u+="&_method="+method;          
+        }
+    }
+    proxied.apply(this, [m, u, user, password]);
+  };
 })();
