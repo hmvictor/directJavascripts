6c2d15055e037595bec3fe7b218acd485324ad04
Victor Herrera <hmvictor@users.noreply.github.com>
2017-07-12 21:44:51 -0500

    Update appContextUrlRewriter.js

--- appContextUrlRewriter.js
+++ appContextUrlRewriter.js
@@ -1,2 +1,20 @@
 (function(){
+  function getAppContext(){
+      //missing
+  }
+  function isAbsolutUrl(url) {
+      //missing
+  }
+  function startsWith(string, prefix){
+      //missing
+  }
+  var proxied=XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open=function(method, url, user, password) {
+    var u=url;
+    var appContext=getAppContext();
+    if(!isAbsolutUrl(url) && !startsWith(appContext)){
+       u=appContext+u;
+    }
+    proxied.apply(this, [method, u, user, password]);
+  };
 })();
